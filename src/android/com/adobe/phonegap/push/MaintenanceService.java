519e41247623a3bb34135285c70cb2674dfb1193
lnpsoft <support@lnpsoft.com>
2016-01-04 03:41:41 +0900

    Create MaintenanceService.java

---
+++ src/android/com/adobe/phonegap/push/MaintenanceService.java
@@ -0,0 +1,113 @@
+package com.adobe.phonegap.push;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by madosaja on 16. 1. 4..
+ */
+public class MaintenanceService extends Service {
+    private static final String LOG_TAG = "MaintenanceService";
+    private static int REBOOT_DELAY_TIMER = 10 * 1000;
+
+    public class LocalBinder extends Binder {
+        public MaintenanceService getService() {
+            return MaintenanceService.this;
+        }
+    }
+    private final IBinder mBinder = new LocalBinder();
+
+    private static final Map<String, String> draftMap = new HashMap<String, String>();
+    static {
+        draftMap.put("draft10", "org.java_websocket.drafts.Draft_10");
+        draftMap.put("draft17", "org.java_websocket.drafts.Draft_17");
+        draftMap.put("draft75", "org.java_websocket.drafts.Draft_75");
+        draftMap.put("draft76", "org.java_websocket.drafts.Draft_76");
+    }
+
+    /**
+     *
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG_TAG, "onBind");
+        return mBinder;
+    }
+
+    /**
+     * 서비스가 만들어졌을 때
+     */
+    @Override
+    public void onCreate() {
+        Log.d(LOG_TAG, "onCreate");
+        unregisterRestartAlarm();
+        super.onCreate();
+    }
+
+    /**
+     * 서비스가 종료될 때
+     */
+    @Override
+    public void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy");
+        registerRestartAlarm();
+        stopForeground(true);
+        super.onDestroy();
+    }
+
+    /**
+     * 서비스를 시작한다면
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onStart(Intent intent, int startId){
+        Log.d(LOG_TAG, "onStart");
+        super.onStart(intent, startId);
+        startForeground(startId, new Notification());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "onStartCommand");
+
+        return START_STICKY;
+    }
+
+    /**
+     * 서비스가 죽었을 때 다시 살리도록 알람설정
+     */
+    private void registerRestartAlarm(){
+        Intent intent = new Intent(MaintenanceService.this, RestartService.class);
+        intent.setAction(RestartService.ACTION_RESTART_CHATDSERVICE);
+        PendingIntent sender = PendingIntent.getBroadcast(MaintenanceService.this, 0, intent, 0);
+
+        long firstTime = SystemClock.elapsedRealtime();
+        firstTime += REBOOT_DELAY_TIMER;
+
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, REBOOT_DELAY_TIMER, sender);
+    }
+
+    /**
+     * 알람설정 해제
+     */
+    private void unregisterRestartAlarm(){
+        Intent intent = new Intent(MaintenanceService.this, RestartService.class);
+        intent.setAction(RestartService.ACTION_RESTART_CHATDSERVICE);
+        PendingIntent sender = PendingIntent.getBroadcast(MaintenanceService.this, 0, intent, 0);
+
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.cancel(sender);
+    }
+
+}
