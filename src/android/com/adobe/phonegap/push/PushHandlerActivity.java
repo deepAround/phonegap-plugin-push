7e8dc48bfcc74ac19660738ed92f1b23d4c0e040
lnpsoft <support@lnpsoft.com>
2016-02-09 23:14:05 +0900

    Update PushHandlerActivity.java

--- src/android/com/adobe/phonegap/push/PushHandlerActivity.java
+++ src/android/com/adobe/phonegap/push/PushHandlerActivity.java
@@ -21,6 +21,7 @@ public class PushHandlerActivity extends Activity implements PushConstants {
     public void onCreate(Bundle savedInstanceState) {
         GCMIntentService gcm = new GCMIntentService();
         gcm.setNotification(getIntent().getIntExtra(NOT_ID, 0), "");
+        clearBadge();
         super.onCreate(savedInstanceState);
         Log.v(LOG_TAG, "onCreate");
 
@@ -66,5 +67,36 @@ public class PushHandlerActivity extends Activity implements PushConstants {
         super.onResume();
         final NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.cancelAll();
+    }
+
+    /**
+     * Persist the badge of the app icon so that `getBadge` is able to return
+     * the badge number back to the client.
+     *
+     * @param badge
+     *      The badge of the app icon
+     */
+    public void saveBadge (int badge) {
+        String packageName = getApplicationContext().getPackageName();
+        Intent launchIntent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(packageName);
+        String className = launchIntent.getComponent().getClassName();
+
+        Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
+        intent.putExtra("badge_count", badge);
+
+        Log.d(LOG_TAG, "saveBadge: " + badge + ", "+packageName+", "+className);
+        // 메인 메뉴에 나타나는 어플의  패키지 명
+        intent.putExtra("badge_count_package_name", packageName);
+        // 메인메뉴에 나타나는 어플의 클래스 명
+        intent.putExtra("badge_count_class_name", className);
+        sendBroadcast(intent);
+    }
+
+    /**
+     * Clears the badge of the app icon.
+     */
+    public void clearBadge() {
+        GCMIntentService.badgeCount = 0;
+        saveBadge(0);
     }
 }
